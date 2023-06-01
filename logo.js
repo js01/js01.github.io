79143df35d82d6cfb7f8b021eaace0edea3ff315
wxcoder <wxcoder@outlook.com>
2023-06-01 11:01:40 +0800

    a

--- logo.js
+++ logo.js
@@ -1,2 +1,2 @@
 const 随机数 = Math.round(Math.random()*2+1)
-document.querySelector('img').src=`https://js01.github.io/images/${随机数}.png`
+document.querySelector('img').src='https://js01.github.io/images/1.png'
