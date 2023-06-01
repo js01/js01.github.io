8996663f4ebb889a5f318f0714b158f7a501aaae
wxcoder <wxcoder@outlook.com>
2023-06-01 11:02:49 +0800

    a

--- logo.js
+++ logo.js
@@ -1,2 +1,2 @@
 const 随机数 = Math.round(Math.random()*2+1)
-document.querySelector('img').src='https://js01.github.io/images/1.png'
+document.querySelector('img').src='https://js01.github.io/images/emoji/1.png'
